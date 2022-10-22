0a234aa4e85e5ff1df3619c15dc6981a1a6ba8ae
Ilya krechin <helium@internet.ru>
2022-10-22 21:36:00 +0300

    creat section skilles

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const hamburder = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
+      counters = document.querySelectorAll('.level__percent');
+      lines = document.querySelectorAll('.level__progress-check');
 
 hamburder.addEventListener('click', () => {
     menu.classList.add('active');
@@ -10,4 +12,6 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
-
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
